dda1c0142f673a7d2a1d63a8a9b69faf9523ed17
RaissaMoreira <raissamoreira80@gmail.com>
2022-04-25 09:45:15 -0300

    first commit(consumindo api)

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './global.css';
+import Home from './pages/home';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,34 @@
+import { Container, MovieList, Movie } from "./styles";
+import { APIKey } from '../../config/key.js';
+import { useState, useEffect } from 'react';
+
+function Home() {
+  const [movies, setMovies] = useState([]);
+  const image_path = 'https://image.tmdb.org/t/p/w500';
+
+  useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`)
+    .then(response => response.json())
+    .then(data => setMovies(data.results))
+  }, [])
+
+  return (
+    <Container>
+      <h1>Rai Movies</h1>
+
+      <MovieList>
+        {movies.map(movie => {
+          return (
+            <Movie key={movie.id}>
+              <a href="https://google.com"><img src={`${image_path}${movie.poster_path}`} alt={movie.title} /></a>
+              <span>{movie.title}</span>
+            </Movie>
+          )
+        })}
+      </MovieList>
+    </Container>
+  )
+}
+
+export default Home;
